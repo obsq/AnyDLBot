61787c87c29859677093954d19ab724f3422a026
obsq <63944348+obsq@users.noreply.github.com>
2020-05-15 08:20:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 1257063
+  API_HASH = "bb2f00cc77b85c6bc375223ba76e0372"
+  TG_BOT_TOKEN = "1170606677:AAHIWFw_x99470TtITNjVOvE4xCdeWlPtu0"
+  AUTH_USERS = [
+      "724495167"
+      # is a string for LEGACY purposes
+  ]
